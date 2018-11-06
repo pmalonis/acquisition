18fce048a715fbfa0ec9bf67f5ccf2ecab13ca29
pmalonis <pmalonis@uchicago.edu>
2018-11-06 16:56:28 -0600

    created test kinect capture script

---
+++ capture_kinect.py
@@ -0,0 +1,21 @@
+from pykinect2 import PyKinectV2
+from pykinect2.PyKinectV2 import *
+from pykinect2 import PyKinectRuntime
+import cv2
+
+class CaptureRuntime(object):
+    def __init__(self, output):
+
+        self._done = False
+        self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color))
+        self.cap = cv2.VideoCapture(output)
+
+    def run(self):
+        while not self._done:
+            if self._kinect.has_new_color_frame():
+                frame = self._kinect.get_last_color_frame()
+                print(frame)
+
+if __name__=='__main__':
+    ob = CaptureRuntime('test.avi')
+    ob.run()
